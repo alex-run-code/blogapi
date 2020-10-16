8f490085da278f94855fb55eef3a7e05533d4982
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-10-16 19:49:15 +0300

    django oauth toolkit installed and working

--- blogapi/blog/views.py
+++ blogapi/blog/views.py
@@ -8,21 +8,6 @@ from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets
 
-#
-# class PostList(generics.ListCreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-#
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
-#
-#
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#     permission_classes = [permissions.IsAuthenticatedOrReadOnly,  IsOwnerOrReadOnly]
-
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'oauth2_provider',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
